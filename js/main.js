fc5b2d377e85233ae3bb4416da17e0097589ef5c
james lawson <jlawson079@gmail.com>
2018-02-27 19:51:05 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var toDo = new Vue({
+	el: "#toDo",
+	data: {
+		newTask: '',
+		todoList: []
+	},
+	methods: {
+		addTask: function(){
+			var task = this.newTask;
+			this.todoList.push(task);
+			this.newTask = '';
+		},
+		removeTask: function(task) {
+			var index = this.todoList.indexOf(task);
+			this.todoList.splice(index, 1);
+		}
+
+	}
+});
